a6668007a19358ca4a0e37b977be45ec44855ab8
[Anup  Magar] <am6956275@gmail.com>
2025-01-13 23:02:48 +0545

    constants abd normalize a words

--- SearchEngineSimulator.c
+++ SearchEngineSimulator.c
@@ -2,3 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+
+// Constants
+int indexSize = 0;
+
+// Normalize a word: convert to lowercase and remove punctuation
+void normalizeWord(char *word) {
+    int len = strlen(word);
+    for (int i = 0; i < len; i++) {
+        word[i] = tolower(word[i]);
+        if (ispunct(word[i])) {
+            word[i] = '\0';
+            break;
+        }
+    }
+}
